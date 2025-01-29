a73a1daf76ee243bc68eb49c9eab69dfe7c3d6b2
Ritik Meshram <160781342+RiTiKM416@users.noreply.github.com>
2025-01-30 02:11:55 +0530

    Create PaymentPage.tsx

---
+++ PaymentPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+const PaymentPage = () => {
+  return (
+    <div className="payment-page">
+      <h1>Payment Gateway</h1>
+      <p>This is where the payment gateway will be integrated.</p>
+    </div>
+  );
+};
+
+export default PaymentPage;
